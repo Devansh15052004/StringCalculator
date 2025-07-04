86381423ce7d1d9395062332947856625b6e2054
Devansh Hapalia <devanshhapalia1554@gmail.com>
2025-07-04 12:21:22 +0530

    Green:two numbers test

--- src/StringCalculator.js
+++ src/StringCalculator.js
@@ -3,6 +3,13 @@ class StringCalculator {
         if (numbers === '') {
             return 0;
         }
+        if (numbers.includes(',')) {
+            const parts = numbers.split(',');
+            for (const part of parts) {
+                sum += parseInt(part);
+            }
+            return sum;
+        }
         return parseInt(numbers); 
     }
 }
