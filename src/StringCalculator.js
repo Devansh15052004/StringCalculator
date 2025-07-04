45fca4718ae9330d20376bc30526a90048b9d3f0
Devansh Hapalia <devanshhapalia1554@gmail.com>
2025-07-04 21:03:22 +0530

    Green: Add support for custom delimiters

--- src/StringCalculator.js
+++ src/StringCalculator.js
@@ -3,10 +3,18 @@ class StringCalculator {
         if (numbers === '') {
             return 0;
         }
+        let delimiter = ',';
+        let numbersString = numbers;
 
-        const newLineSupportNumber = numbers.replace(/\n/g, ',');
+        if (numbers.startsWith('//')) {
+            const delimiterEndIndex = numbers.indexOf('\n');
+            delimiter = numbers.substring(2, delimiterEndIndex);
+            numbersString = numbers.substring(delimiterEndIndex + 1);
+        }
+        
+        const newLineSupportNumber = numbersString.replace(/\n/g, delimiter);
 
-        const parts = newLineSupportNumber.split(',');
+        const parts = newLineSupportNumber.split(delimiter);
         let sum = 0;
         for (const part of parts) {
             sum += parseInt(part);
