333da37f0fce20a509efe8215dbcc8eacf56fd76
Devansh Hapalia <devanshhapalia1554@gmail.com>
2025-07-05 14:45:58 +0530

    Refactor : for better readability and maintainability

--- src/StringCalculator.js
+++ src/StringCalculator.js
@@ -1,35 +1,53 @@
 class StringCalculator {
-    add(numbers) {
-        if (numbers === '') {
-            return 0;
-        }
-        let delimiter = ',';
-        let numbersString = numbers;
+  add(numbers) {
+    if (numbers === "") {
+      return 0;
+    }
+    const { delimiter, numbersString } = this.extractDelimiterAndNumbers(numbers);
 
-        if (numbers.startsWith('//')) {
-            const delimiterEndIndex = numbers.indexOf('\n');
-            delimiter = numbers.substring(2, delimiterEndIndex);
-            numbersString = numbers.substring(delimiterEndIndex + 1);
-        }
+    const normalizedNumbers = this.normalizeDelimiters(
+      numbersString,
+      delimiter
+    );
+
+    const parts = normalizedNumbers.split(delimiter);
         
-        const newLineSupportNumber = numbersString.replace(/\n/g, delimiter);
+    return this.calculateSum(parts);
+  }
 
-        const parts = newLineSupportNumber.split(delimiter);
+  extractDelimiterAndNumbers(numbers) {
+    if (numbers.startsWith("//")) {
+      const delimiterEndIndex = numbers.indexOf("\n");
+      delimiter = numbers.substring(2, delimiterEndIndex);
+      numbersString = numbers.substring(delimiterEndIndex + 1);
+      return { delimiter, numbersString };
+    }
+    return { delimiter: ",", numbersString: numbers };
+  }
+
+  normalizeDelimiters(numbersString,delimiter){
+    return numbersString.replace(/\n/g, delimiter);
+  }
+
+  calculateSum(parts) {
         const negativeNumbers = [];
         let sum = 0;
+        
         for (const part of parts) {
             const number = parseInt(part);
-             if (number < 0) {
+            if (number < 0) {
                 negativeNumbers.push(number);
             } else {
                 sum += number;
             }
         }
+        
         if (negativeNumbers.length > 0) {
             throw new Error(`negative numbers not allowed ${negativeNumbers.join(',')}`);
         }
-        return sum; 
+        
+        return sum;
     }
 }
 
-module.exports = StringCalculator; 
+module.exports = StringCalculator;
